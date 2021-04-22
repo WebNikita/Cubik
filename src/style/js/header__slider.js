858ee17576a04096899ffa34445c20338ff6210c
Prom1x <kirill.zhu46@gmail.com>
2021-04-22 17:46:22 +0300

    catalog_slider

--- src/style/js/header__slider.js
+++ src/style/js/header__slider.js
@@ -3,11 +3,21 @@ import '@/scss/main.scss';
 
 $('.header').hover(
     (function() {
-      console.log('ININ');
-      $('.header').addClass('hover');
+      $('.header__slider').css({
+        'display': 'block',
+        'margin': '0px'});
+      $('ul').css('margin', '0');
     }),
     function() {
-      console.log('OUTPUT');
-      $(this).removeClass('hover');
+      $('.header__slider').css('display', 'none');
+    }
+);
+
+$('#1').hover(
+    function() {
+      $('.dev').css('display', 'block');
+    },
+    function() {
+      $('.dev').css('display', 'none');
     }
 );
