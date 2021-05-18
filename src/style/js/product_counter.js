69f595ed5b5d67e3fb53872e49c6d320e538ce2d
Prom1x <kirill.zhu46@gmail.com>
2021-05-18 15:10:54 +0300

    edit product(script for counter)

--- src/style/js/product_counter.js
+++ src/style/js/product_counter.js
@@ -1,6 +1,38 @@
 import * as $ from 'jquery';
+let count = 0;
+const text = $('.block__product_third-top_price').prop('innerText');
+const inter = parseInt(text);
 
-$('.button_blur').click(function() {
-  $('.container__product_info').toggleClass('blur');
-  $(this).children().toggleClass('rotate');
+$('.plus').click(function() {
+  count++;
+  const price =inter*count;
+  $('.end_price').html(price + ' Р');
+  $('.count').html(count);
+  console.log(count);
+  console.log(price);
 });
+
+$('.minus').click(function() {
+  if (count >= 1) {
+    const end = $('.end_price').prop('innerText');
+    const price = parseInt(end);
+    count--;
+    const priceEnd = price-inter;
+    $('.end_price').html(priceEnd + ' Р');
+    $('.count').html(count);
+    console.log(count);
+    console.log(price);
+  }
+});
+
+// let counter = 0;
+// counter += inter;
+// console.log(counter);
+// counter += inter;
+// console.log(counter);
+// counter += inter;
+// console.log(text);
+
+
+// console.log(inter);
+// console.log(counter);
